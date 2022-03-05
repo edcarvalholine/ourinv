3a218bbcc3750075eb7b738bda487ecdbcbb1503
edcarvalholine <edcarvalholine@gmail.com>
2022-03-05 16:15:03 +0000

    Added product operations

--- ourinv.WebAPI/Controllers/ProductController.cs
+++ ourinv.WebAPI/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ourinv.WebAPI.Database;
+using ourinv.WebAPI.DTOs.CategoryDTO;
 using ourinv.WebAPI.DTOs.ProductDTO;
 using ourinv.WebAPI.Models;
 
@@ -23,6 +24,42 @@ namespace ourinv.WebAPI.Controllers
             var existingProduct = getProductByName(productName);
 
             return Ok(convertToDTO(existingProduct));
+        }
+
+        [HttpGet("Category/{categoryName}")]
+        public IActionResult GetProductsFromCategory(string categoryName)
+        {
+            var existingProductWithCategories = getProductsFromCategoryName(categoryName);
+
+            return Ok(existingProductWithCategories);
+        }
+
+        [HttpPost]
+        public IActionResult CreateProdct(string? categoryName, string productName, int productQuantity)
+        {
+            if (categoryName == null)
+            {
+                categoryName = "Unknown";
+            }
+
+            var productCategory = _context.Categories.SingleOrDefault(x => x.Name == categoryName);
+            var existingProduct = _context.Products.Include(x => x.Category).SingleOrDefault(x => x.Name == productName);
+            if (existingProduct != null)
+            {
+                throw new Exception($"The product with name {productName} already exists in category {categoryName}!");
+            }
+
+            Product newProduct = new()
+            {
+                Name = productName,
+                Quantity = productQuantity,
+                Category = productCategory
+            };
+
+            _context.Products.Add(newProduct);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetProduct), new { productName = newProduct.Name }, newProduct);
         }
 
         private BaseProductDTO convertToDTO(Product product)
@@ -48,6 +85,28 @@ namespace ourinv.WebAPI.Controllers
             }
 
             return existingProduct;
+        }
+
+        private BaseCategoryDTO getProductsFromCategoryName(string categoryName)
+        {
+            var existingCategory = _context.Categories.Include(x => x.Products).Where(x => x.Name == categoryName).SingleOrDefault();
+            if (existingCategory == null)
+            {
+                throw new Exception($"The categoryName with name {categoryName} doesn't exist!");
+            }
+
+
+            List<BaseProductDTO> formattedProducts = new();
+            foreach (var product in existingCategory.Products)
+            {
+                formattedProducts.Add(new() {  Name = product.Name, Quantity = product.Quantity });
+            }
+
+            return new()
+            {
+                Name = existingCategory.Name,
+                Products = formattedProducts 
+            };
         }
     }
 }
--- ourinv.WebAPI/Program.cs
+++ ourinv.WebAPI/Program.cs
@@ -16,15 +16,16 @@ using (var serviceScope = app.Services.CreateScope())
 {
     var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
     context.Categories.AddRange(
-        new() { Id = 1, Name = "Vegetais" },
-        new() { Id = 2, Name = "Carnes" }
+        new() { Name = "Unknown" },
+        new() { Name = "Vegetais" },
+        new() { Name = "Carnes" }
     );
     context.SaveChanges();
     context.Products.AddRange(
-        new() { Category = context.Categories.Where(x => x.Id == 1).Single(), Id = 1, Name = "Bróculos", Quantity = 10 },
-        new() { Category = context.Categories.Where(x => x.Id == 1).Single(), Id = 2, Name = "Batatas", Quantity = 3 },
-        new() { Category = context.Categories.Where(x => x.Id == 2).Single(), Id = 3, Name = "Vaca", Quantity = 5 },
-        new() { Id = 4, Name = "Porco", Quantity = 3 }
+        new() { Category = context.Categories.Find(2), Id = 1, Name = "Bróculos", Quantity = 10 },
+        new() { Category = context.Categories.Find(2), Id = 2, Name = "Batatas", Quantity = 3 },
+        new() { Category = context.Categories.Find(3), Id = 3, Name = "Vaca", Quantity = 5 },
+        new() { Category = context.Categories.Find(1), Id = 4, Name = "Porco", Quantity = 3 }
     );
     context.SaveChanges();
 
